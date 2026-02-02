4aec8aa51d4cad8bc4dfb9b4b119e433178b0fb9
Aman J <jamanrao@gmail.com>
2026-02-02 17:44:06 +0530

    i completed the basic options for tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App()
+{
+    return (<div>
+        <div id="option">Choose one of the following symbols to play</div>
+        <div id="both">
+            <div id="X">X</div>
+            <div id="O">O</div>
+        </div>
+    </div>); 
 }
 
-export default App;
+
